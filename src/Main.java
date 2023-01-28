966c8c11d005d3da41b360e32784dd0f133becdf
Ivan Pozdov <Ivan.pozdov58@gmail.com>
2023-01-28 20:50:17 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ public class Main {
 int balance = 100;  //Стартовая сумма на счету.
 int added = 1100;  //Сумма пополнения.
 
-    int bonus = 0;
-    if (added > 1000) {
-        bonus = added / 100;
-    }
+        int bonus = 0;
+    if (added > 1000) bonus = added / 100;
     int total = balance + added + bonus;
     System.out.println ("Итоговая сумма на счету клиента:" + total + "рублей.");
         System.out.println("Кол-во бонусов: " + bonus);
