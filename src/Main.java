214deb31dad071ff8bfe40fb1342b15eae487299
Ivan Pozdov <Ivan.pozdov58@gmail.com>
2023-02-01 23:19:45 +0300

    remove commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-int balance = 100;  //Стартовая сумма на счету.
-int added = 1100;  //Сумма пополнения.
-
+        int balance = 100;  //Стартовая сумма на счету.
+        int added = 1100;  //Сумма пополнения.
         int bonus = 0;
         bonus = added / 100;
         int total = balance + added + bonus;
